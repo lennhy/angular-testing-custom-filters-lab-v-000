8086294e28615123d497c527c8fc07d1544da604
lenn hypolite <lennhydesign@gmail.com>
2016-12-13 13:12:48 -0500

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,21 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+	it('should filter on favorite food correctly', function () {
+		var mockedList = [{
+			name: 'Test',
+			favoriteFood: 'Bread'
+		}, {
+			name: 'Test 2',
+			favoriteFood: 'Apples'
+		}, {
+			name: 'Test 3',
+			favoriteFood: 'Carrots'
+		}];
+
+		var results = $filter('favoriteFood')(mockedList, 'Apples');
+
+		expect(results.length).toBe(1);
+		expect(results[0].name).toBe('Test 2');
+	});
 });
